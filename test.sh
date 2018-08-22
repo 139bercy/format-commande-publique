515645d8d7295cd8213360653810d283d8f4ab06
Colin Maudry <colin@maudry.com>
2018-08-22 16:53:23 +0200

    Test des schémas et exemples XML #2

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #/bin/bash
 
-# Ce script sert à tester la validité des fichiers d'exemples par rapport aux schémas. Il teste exemples JSON.
+# Ce script sert à tester la validité des fichiers d'exemples par rapport aux schémas. Il teste les exemples JSON et XML.
 
-function testExempleJson {
+base=`pwd`
+
+
+
+function testExemplesJson {
     prefixe=$1
     schema=$2
     for exemple in `ls $prefixe*`
     do
-        echo "  $exemple ($schema)"
+        echo "$exemple ($schema)"
         ajv -s ../../schémas/json/$schema -d $exemple > testlog
         if [ `head -n 1 testlog | grep invalid` ]
         then
@@ -16,17 +20,65 @@ function testExempleJson {
             echo "> OK"
         fi
         echo ""
+        rm testlog
+    done
+}
+
+function testSchémasXml {
+    for xsd in `ls *.xsd`
+    do
+        echo "$xsd"
+        xmllint --noout --schema $base/test/XMLSchema.xsd $xsd 2> testlog
+        if [[ $(< testlog) == "$xsd validates" ]]
+        then
+            echo "> OK"
+        else
+            cat testlog
+        fi
+        echo ""
+        rm testlog
+    done
+}
+
+function testExemplesXml {
+    prefixe=$1
+    schema=$2
+    for exemple in `ls $prefixe*`
+    do
+        echo "$exemple ($schema)"
+        java -jar $base/test/xsd11-validator.jar -sf ../../schémas/xml/$schema -if $exemple 2> testlog
+        if [ -s testlog ]
+        then
+            cat testlog
+        else
+            echo "> OK"
+        fi
+        echo ""
+        rm testlog
     done
 }
 
 echo "Test des exemples JSON"
 echo ""
 
-cd exemples/json
+cd $base/exemples/json
 
-testExempleJson "mar" "marchés.json"
-testExempleJson "cc" "contrats-concessions.json"
-testExempleJson "mds" "marchés-défense-sécurité.json"
+testExemplesJson "mar" "marchés.json"
+testExemplesJson "cc" "contrats-concessions.json"
+testExemplesJson "mds" "marchés-défense-sécurité.json"
 
+echo "Validation des schémas XML"
+echo ""
 
-rm testlog
+cd $base/schémas/xml
+
+testSchémasXml
+
+echo "Test des exemples XML"
+echo ""
+
+cd $base/exemples/xml
+
+testExemplesXml "mar" "marchés.xsd"
+testExemplesXml "cc" "contrats-concessions.xsd"
+testExemplesXml "mds" "marchés-défense-sécurité.xsd"
