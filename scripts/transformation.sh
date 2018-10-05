6a70312efc5acd6e1e19c7172642bf8ce107193f
Colin Maudry <colin@maudry.com>
2018-10-05 18:25:53 +0200

    Clarification des retours console/log
    Mais réencodage non-fonctionnel

--- scripts/transformation.sh
+++ scripts/transformation.sh
@@ -14,40 +14,53 @@ zips=$1
 mkdir -p $racine
 
 
-echo "\n\nMise en lecture seule des ZIPs et décompression des fichiers XML vers l'historique..."
+echo -e "\033[1m\n\nMise en lecture seule des ZIPs et décompression des fichiers XML vers l'historique...\033[0m"
 
-echo "Répertoire courant (zips) : $zips"
+echo -e "Répertoire courant (zips) : $zips"
 cd $zips
+ls -alF --color -h --group-directories-first
 chmod 444 *.zip
 mkdir -p $racine/historiqueXML/$date
-unzip '*.zip' -d $racine/historiqueXML/$date
+unzip -q '*.zip' -d $racine/historiqueXML/$date
 
-echo "\n\nCopie des fichiers XML vers un dossier temporaire..."
+echo -e "\033[1m\n\nCopie des fichiers XML vers un dossier temporaire...\033[0m"
 
-echo "Répertoire courant (racine) : $racine"
+echo -e "Répertoire courant (racine) : $racine"
 cd $racine
 
-mkdir temp
+if [ ! -d temp ]; then
+    mkdir temp
+fi
+
 cp -rv historiqueXML/$date/* temp/
 
-echo "\n\nDéplacement des fichiers ZIP vers l'historique ZIP..."
+echo -e "\033[1m\n\nDéplacement des fichiers ZIP vers l'historique ZIP...\033[0m"
 
 mkdir -p historiqueZIP/$date
 mv -v $zips/*.zip historiqueZIP/$date
 
-echo "\n\nRéencodage en UTF-8 et transformation des fichiers XML..."
+echo -e "\033[1m\n\nRéencodage en UTF-8 et transformation des fichiers XML...\033[0m"
 
 cd temp
+vides=0
+pasvides=0
 for xml in `find -path **/*.xml`
 do
-iconv -f ISO-8859-1 -t UTF-8 $xml -o $xml.utf8.xml
-mv $xml.utf8.xml $xml
-java -jar $saxonJar -s:$xml -xsl:$scriptDir/decpDGFIP.xsl
+    if [ -s $xml ]; then
+        echo -e "$xml : réencodage et transformation"
+        iconv --verbose -f ISO-8859-1 -t UTF-8 $xml | java -jar $saxonJar -s:- -xsl:$scriptDir/decpDGFIP.xsl
+        ((pasvides++))
+    else
+        echo -e "$xml : fichier vide"
+        ((vides++))
+    fi
 done
 
-# echo "Fusion des fichiers XML (quand c'est nécessaire)..."
+total=$((pasvides + vides))
+echo -e "\nFichiers traités : $pasvides Fichiers vides : $vides Total : $total"
 
-echo "Fusion et dédoublonnage des données urlProfilAcheteur..."
+# echo -e "Fusion des fichiers XML (quand c'est nécessaire)..."
+
+echo -e "\033[1m\n\nFusion et dédoublonnage des données urlProfilAcheteur...\033[0m"
 mkdir -p $racine/profilsAcheteurs
 sort `ls sirets/$annee/$mois/$jour/*.xml/*` | uniq > $racine/profilsAcheteurs/profilsAcheteurs-$date.csv
-
